7ae3567ba1a1ab9bd8b2070d62e19658909f35d3
Thembelisha <u25224663@tuks.co.za>
2026-05-25 13:02:51 +0200

    .

--- agency/agency_header.php
+++ agency/agency_header.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 
 
 if (!isset($_SESSION['agency_id'])) {
-  header("Location: ../login.php");
+  header("Location: FantasticFive\Traveller\login.php");
   exit();
 }
 $agency_name = $_SESSION['agency_name'] ?? 'Travel Partner';
